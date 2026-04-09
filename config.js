7cce485489be340757090515fea934dbab3f7d87
nadiadagelin-lang <nadia.dagelin@gmail.com>
2026-04-09 08:15:37 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.CHERRY_CHASE_CONFIG = {
   scoreboard: {
     provider: "supabase",
-    url: "",
-    anonKey: "",
+    url: "https://heaqlxufiyppsmlupdha.supabase.co",
+    anonKey: "sb_publishable_uiyvunlJbfPlrLyQtyLJZQ_qarN-NJU",
     table: "cherry_chase_scores"
   }
 };
